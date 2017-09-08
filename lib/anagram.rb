fdf4fbe93ac25cff2e08f0b568b5999f1b40e9e7
Rafael Furry <rfurry@gmail.com>
2017-09-08 09:52:13 -0700

    have anagram method account for cases

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,10 @@
 def anagram(str1, str2)
     str1 = str1.downcase
     str2 = str2.downcase
-
     arr1 = str1.split("")
     arr2 = str2.split("")
-
     arr1 = arr1.sort
     arr2 = arr2.sort
-
     if (arr1 <=> arr2) == 0
         return true
     else
